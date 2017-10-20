09d8fcfff8f26476cdca640beb8394eacdfb021b
Aleksandr Guidrevitch <aguidrevitch@gmail.com>
2017-10-20 21:57:15 +0300

    better cache class

--- cache.php
+++ cache.php
@@ -3,40 +3,74 @@
 /**
  * Class the_ultimate_cache_cache
  * @property the_ultimate_cache_backend $backend
+ * @property array() $server
+ * @property array() $rules
  */
-class the_ultimate_cache_cache {
+class the_ultimate_cache_cache
+{
 
     protected $collected = '';
 
     protected $backend;
     protected $server;
+    protected $rules;
 
-    public function __construct($backend, $server) {
-        $this->backend     = $backend;
-        $this->server      = $server;
+    protected $expires;
+
+    public function __construct($backend, $server, $rules = array())
+    {
+        $this->backend = $backend;
+        $this->server = $server;
+        $this->rules = $rules;
     }
 
-    protected function get_key() {
+    protected function get_key()
+    {
         // TODO: apply rules (strip QS if needed)
         // TODO: apply rules (cache with QS but ignore QS param order)
         return @$this->server['REQUEST_METHOD'] . "|" . @$this->server['REQUEST_URI'];
     }
 
-    protected function request_can_be_cached() {
+    protected function request_can_be_cached()
+    {
         if (preg_match('/wordpress_logged_in_/', @$this->server['HTTP_COOKIE'])) {
             return false;
         }
-        // TODO: apply rules
-        // TODO: allow to cache POST
-        return preg_match('/^(GET|HEAD)$/i', @$this->server['REQUEST_METHOD']);
+        if ($this->expires && $this->expires <= time()) {
+            return false;
+        }
+        return preg_match('/^(GET|HEAD)$/i', $this->server['REQUEST_METHOD']);
     }
 
-    protected function response_can_be_cached() {
+    protected function response_can_be_cached()
+    {
         // make configurable
         return http_response_code() >= 200 && http_response_code() < 300;
     }
 
-    public function handler() {
+    public function handler()
+    {
+        if (isset($this->rules['request'])) {
+            foreach ($this->rules['request'] as $rule) {
+                if (substr($rule['url'], -1) == '*') {
+                    $re = '/^' . preg_replace('/\\\\\*/', '.*?', preg_quote($rule['url'], '/')) . '/';
+                } else {
+                    $re = '/^' . preg_replace('/\\\\\*/', '.*?', preg_quote($rule['url'], '/')) . '$/';
+                }
+                $uri = preg_replace('/\?.*/', '', $this->server['REQUEST_URI']);
+                /**
+                 * ttl
+                 *      -1 Never
+                 *       0+ Seconds
+                 *    null Forever
+                 */
+                if (preg_match($re, $uri) && $rule['ttl']) {
+                    $this->expires = time() + $rule['ttl'];
+                    break;
+                }
+            }
+        }
+
         if (!$this->request_can_be_cached())
             return false;
 
@@ -46,6 +80,7 @@ class the_ultimate_cache_cache {
                 foreach ($cached['headers'] as $header) {
                     header($header);
                 };
+                header('X-Cached-By: https://wordpress.org/plugins/the-ultimate-cache');
                 echo $cached['body'];
                 exit;
             }
@@ -54,11 +89,13 @@ class the_ultimate_cache_cache {
         register_shutdown_function(array($this, 'shutdown'));
     }
 
-    public function collect($buffer) {
+    public function collect($buffer)
+    {
         $this->collected .= $buffer;
     }
 
-    public function shutdown() {
+    public function shutdown()
+    {
         if ($this->response_can_be_cached()) {
             @header_remove('Set-Cookie');
             @header_remove('Set-Cookie2');
@@ -70,12 +107,13 @@ class the_ultimate_cache_cache {
                 'headers' => headers_list(),
                 'body' => $this->collected,
             );
-            $this->backend->store($this->get_key(), serialize($cached));
+            $this->backend->store($this->get_key(), serialize($cached), $this->expires);
         }
         echo $this->collected;
     }
 
-    public function invalidate($urls) {
+    public function invalidate($urls)
+    {
         return $this->backend->invalidate($urls);
     }
 }
